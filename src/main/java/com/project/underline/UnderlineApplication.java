d03445a2c3cd4fb42f355b453a4e3f543bc1f71e
wanjongth <53491653+wanjongth@users.noreply.github.com>
2022-09-27 19:55:20 +0900

    dev settings

---
+++ src/main/java/com/project/underline/UnderlineApplication.java
@@ -0,0 +1,13 @@
+package com.project.underline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnderlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/underline/UnderlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.underline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
