554bb0a24f1c4569cbd86ba0aa3948f0a3ffb528
wanjongth <53491653+wanjongth@users.noreply.github.com>
2022-10-05 21:06:37 +0900

    Common Exception Response 처리

--- src/main/java/com/project/underline/bookmark/web/controller/DemoController.java
+++ src/main/java/com/project/underline/bookmark/web/controller/DemoController.java
@@ -2,7 +2,7 @@ package com.project.underline.bookmark.web.controller;
 
 import com.project.underline.bookmark.web.dto.BookmarkRequest;
 import com.project.underline.common.DefaultRes;
-import com.project.underline.common.metadata.ResponseMessage2;
+import com.project.underline.common.metadata.ResponseMessage;
 import com.project.underline.common.metadata.StatusCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,21 +10,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class BookmarkController {
+public class DemoController {
 
     @GetMapping("/test")
     public ResponseEntity test() {
         BookmarkRequest bookmarkRequest = new BookmarkRequest();
 
-        return new ResponseEntity(
-                // enum + get value
-                new DefaultRes<>(StatusCode.OK, ResponseMessage2.CREATED_USER.getValue(), bookmarkRequest), HttpStatus.OK
+        String temp = "";
+        // 예외에 따른 분기
+        if ("".equals(temp)) {
+            throw new IllegalArgumentException(ResponseMessage.NOT_FOUND_USER);
+        }
 
+        return new ResponseEntity(
                 // class + final String + Constructor
 //                new DefaultRes<>(StatusCode.OK, ResponseMessage.CREATED_USER, bookmarkRequest), HttpStatus.OK
 
                 // class + final String + builder
-//                DefaultRes.res(StatusCode.OK, ResponseMessage.CREATED_USER, bookmarkRequest), HttpStatus.OK
+                DefaultRes.res(StatusCode.OK, ResponseMessage.CREATED_USER, bookmarkRequest), HttpStatus.OK
         );
     }
 
--- src/main/java/com/project/underline/common/DefaultRes.java
+++ src/main/java/com/project/underline/common/DefaultRes.java
@@ -1,5 +1,6 @@
 package com.project.underline.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,7 +8,9 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL) // error 처리 시 data : 없앰
 public class DefaultRes<T> {
+    private Class exception;
     private int statusCode;
     private String message;
     private T data;
@@ -25,6 +28,14 @@ public class DefaultRes<T> {
     public static<T> DefaultRes<T> res(final int statusCode, final String message, final T t) {
         return DefaultRes.<T>builder()
                 .data(t)
+                .statusCode(statusCode)
+                .message(message)
+                .build();
+    }
+
+    public static<T> DefaultRes<T> errRes(final int statusCode, final Class exception, final String message) {
+        return DefaultRes.<T>builder()
+                .exception(exception)
                 .statusCode(statusCode)
                 .message(message)
                 .build();
