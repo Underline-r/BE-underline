5ecca80e5497e5b857b7999f658d04c18ebd28ea
gayeonwon <henzel1013@gmail.coom>
2022-11-08 15:34:11 +0900

    [Dto 네이밍 컨벤션 수정] RequestDto -> Request (DTO생략)

---
+++ src/main/java/com/project/underline/post/web/dto/PostRequest.java
@@ -0,0 +1,15 @@
+package com.project.underline.post.web.dto;
+
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class PostRequest {
+
+    private String title;
+    private String content;
+    private String contentType;
+    private List<String> hashtag;
+}
