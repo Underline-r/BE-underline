9cd0ee03dc7a3534d4c432cb3e9beee3df6e7337
gayeonwon <henzel1013@gmail.coom>
2022-11-14 19:32:57 +0900

    [게시글 작성 컨트롤러] Post 엔티티에 timeStamp 적용

--- src/main/java/com/project/underline/post/entity/Post.java
+++ src/main/java/com/project/underline/post/entity/Post.java
@@ -1,15 +1,17 @@
 package com.project.underline.post.entity;
 
+import com.project.underline.common.util.BaseTimeEntity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Date;
 
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Getter
 @Table(name = "Post")
-public class Post {
+public class Post extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
@@ -29,6 +31,9 @@ public class Post {
     @Column(name="CONTENT")
     @Lob
     private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
 
     @Builder
     public Post(Long userId,String title,ContentType contentType,String content){
