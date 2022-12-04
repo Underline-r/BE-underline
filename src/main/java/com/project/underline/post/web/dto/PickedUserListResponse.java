2791b1cae2d37516458bba2267712a391f3ee1ee
Won Gayeon <57881683+silano08@users.noreply.github.com>
2022-12-04 17:46:19 +0900

    [좋아요 컨트롤러] List 생성자 추가

--- src/main/java/com/project/underline/post/web/dto/PickedUserListResponse.java
+++ src/main/java/com/project/underline/post/web/dto/PickedUserListResponse.java
@@ -3,6 +3,7 @@ package com.project.underline.post.web.dto;
 import com.project.underline.user.entity.User;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -11,6 +12,7 @@ public class PickedUserListResponse {
     private List<String> userNicknameList;
 
     public void pickUserListUp(List<User> userList){
+        this.userNicknameList = new ArrayList<String>();
         // 닉네임을 return 해야되니 Pick 객체 -> User 객체 변환은 서비스단에서 해결
         for (User eachPicked : userList) {
             this.userNicknameList.add(eachPicked.getNickname());
