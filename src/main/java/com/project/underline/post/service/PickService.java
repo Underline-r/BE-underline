356fe0355bc2e778809a53ff4f04d55d4b61f970
Won Gayeon <57881683+silano08@users.noreply.github.com>
2022-12-10 14:14:29 +0900

    [연관관계 설정] Pick -> User 추가

--- src/main/java/com/project/underline/post/service/PickService.java
+++ src/main/java/com/project/underline/post/service/PickService.java
@@ -1,16 +1,16 @@
 package com.project.underline.post.service;
 
+import com.project.underline.common.exception.UnderlineException;
+import com.project.underline.common.metadata.ErrorCode;
 import com.project.underline.common.util.SecurityUtil;
 import com.project.underline.post.entity.Pick;
 import com.project.underline.post.entity.repository.PickRepository;
 import com.project.underline.post.web.dto.PickRequest;
 import com.project.underline.post.web.dto.PickedUserListResponse;
-import com.project.underline.user.entity.User;
 import com.project.underline.user.entity.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +19,7 @@ import java.util.Optional;
 public class PickService {
     private final PickRepository pickRepository;
     private final UserRepository userRepository;
+
     public void checkUserPostPick(PickRequest pickRequest) {
         // 해당 게시글을 좋아요했는지 확인
         Optional<Pick> checkAlreadyPicked = pickRepository.findByPostIdAndUserId(pickRequest.getPostId(), SecurityUtil.getCurrentUserId());
@@ -32,7 +33,8 @@ public class PickService {
         pickRepository.save(
                 Pick.builder()
                         .postId(pickRequest.getPostId())
-                        .userId(SecurityUtil.getCurrentUserId())
+                        .user(userRepository.findById(SecurityUtil.getCurrentUserId())
+                                .orElseThrow(() -> new UnderlineException(ErrorCode.CANNOT_FOUND_USER)))
                         .build()
         );
     }
@@ -41,13 +43,7 @@ public class PickService {
         PickedUserListResponse pickedUserListResponse = new PickedUserListResponse();
 
         List<Pick> pickedList = pickRepository.findAllByPostId(postId);
-        List<User> pickedUserList = new ArrayList<User>();
-
-        for (Pick eachPick: pickedList) {
-            pickedUserList.add(userRepository.findById(eachPick.getUserId()).get());
-        }
-
-        pickedUserListResponse.pickUserListUp(pickedUserList);
+        pickedUserListResponse.pickUserListUp(pickedList);
 
         return pickedUserListResponse;
     }
