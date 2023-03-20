e1f2319552c8a4548f10a662f5f02d941909e885
Won Gayeon <57881683+silano08@users.noreply.github.com>
2023-03-21 01:01:43 +0900

    웹 피드 로직 변경

--- src/main/java/com/project/underline/feed/service/WebFeedService.java
+++ src/main/java/com/project/underline/feed/service/WebFeedService.java
@@ -3,14 +3,20 @@ package com.project.underline.feed.service;
 import com.project.underline.category.entity.UserCategoryRelation;
 import com.project.underline.category.entity.repository.UserCategoryRelationRepository;
 import com.project.underline.common.exception.UnderlineException;
+import com.project.underline.common.jwt.JwtFilter;
+import com.project.underline.common.jwt.JwtTokenProvider;
 import com.project.underline.common.util.SecurityUtil;
 import com.project.underline.feed.entity.repository.FeedQueryRepository;
 import com.project.underline.feed.web.dto.FeedResponse;
 import com.project.underline.post.entity.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,39 +25,63 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class WebFeedService {
-    private final PostRepository postRepository;
     private final UserCategoryRelationRepository userCategoryRelationRepository;
     private final FeedQueryRepository feedQueryRepository;
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Transactional
     public FeedResponse makeWebFeed(Pageable pageable) {
         FeedResponse feedResponse = new FeedResponse();
+        Long userId = SecurityUtil.getCurrentUserId();
+
         try {
-            Long userId = SecurityUtil.getCurrentUserId();
+            return loginUserFeed(feedResponse, pageable, userId);
+        } catch (UnderlineException e) {
+            throw e;
+        }
+    }
 
-            // TODO. try-catch 말고 다른 예외처리로 로그인하지 않은 사용자가 요청할때 getDefaultFeedInformation로 넘어가도록 수정 (지금은 동작 자체는 합니다)
-            Optional<List<UserCategoryRelation>> likedCategoryCodeList = userCategoryRelationRepository
-                    .findAllByUserId(userId);
+    private Long tokenReceive(HttpServletRequest request) {
 
-            if(likedCategoryCodeList==null || likedCategoryCodeList.get().size() < 1){
-                feedQueryRepository.getDefaultFeedInformation(feedResponse,pageable);
-            }else{
-                feedQueryRepository.getCustomFeedInformation(feedResponse,pageable, categoryListToString(likedCategoryCodeList.get()));
-            }
-            feedQueryRepository.setUserPickOrBookmark(feedResponse);
-        }catch (UnderlineException e){
-            feedQueryRepository.getDefaultFeedInformation(feedResponse,pageable);
+        String accessToken = request.getHeader(JwtFilter.AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(accessToken) && accessToken.startsWith(JwtFilter.BEARER_PREFIX)) {
+            accessToken.substring(7);
         }
 
-        feedResponse.isLastCheck();
+        if (accessToken != null) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            Long userId = SecurityUtil.getCurrentUserId();
+            return userId;
+        } else {
+            return null;
+        }
+    }
+
+    private FeedResponse notLoginUserFeed(FeedResponse feedResponse, Pageable pageable) {
+        feedQueryRepository.getDefaultFeedInformation(feedResponse, pageable);
+        return feedResponse;
+    }
+
+    private FeedResponse loginUserFeed(FeedResponse feedResponse, Pageable pageable, Long userId) {
+        Optional<List<UserCategoryRelation>> likedCategoryCodeList = userCategoryRelationRepository
+                .findAllByUserId(userId);
+
+        if (likedCategoryCodeList == null || likedCategoryCodeList.get().size() < 1) {
+            feedQueryRepository.getDefaultFeedInformation(feedResponse, pageable);
+        } else {
+            feedQueryRepository.getCustomFeedInformation(feedResponse, pageable, categoryListToString(likedCategoryCodeList.get()));
+        }
+        feedQueryRepository.setUserPickOrBookmark(feedResponse);
 
         return feedResponse;
     }
 
-    private List<String> categoryListToString(List<UserCategoryRelation> categoryList){
+    private List<String> categoryListToString(List<UserCategoryRelation> categoryList) {
         List<String> stringCategoryList = new ArrayList<String>();
 
-        for (UserCategoryRelation eachCategory: categoryList) {
+        for (UserCategoryRelation eachCategory : categoryList) {
             stringCategoryList.add(eachCategory.getCategory());
 
         }
