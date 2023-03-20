39f4103ee42976e05fb9e0c73988256f2bab6046
Won Gayeon <57881683+silano08@users.noreply.github.com>
2023-03-20 17:39:18 +0900

    객체 데이터 추가

--- src/main/java/com/project/underline/post/web/dto/PostDetailResponse.java
+++ src/main/java/com/project/underline/post/web/dto/PostDetailResponse.java
@@ -1,12 +1,33 @@
 package com.project.underline.post.web.dto;
 
 import com.project.underline.post.entity.Post;
+import lombok.Data;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 public class PostDetailResponse {
+    private Long postId;
     private String content;
+    private String reference;
+    private LocalDateTime createdDate;
+
     private Long userId;
+    private String userNickname;
+    private String userProfileImage;
+
+    private Long viewCount;
+    private Long likeCount;
+    private Long commentCount;
+
+    private Long isPicked;
+    private Long isBookmarked;
+    private Long isFollwed;
+
+    private List<String> hashtags;
+    private String oldestComment;
 
     public PostDetailResponse(Post post){
         this.content = post.getContent();
