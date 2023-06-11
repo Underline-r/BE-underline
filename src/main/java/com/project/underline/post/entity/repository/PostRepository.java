052521e0b06ccd0d567a61ce3951dcbc08ae4e02
Won Gayeon <57881683+silano08@users.noreply.github.com>
2023-06-11 10:39:27 +0900

    머지 수정

--- src/main/java/com/project/underline/post/entity/repository/PostRepository.java
+++ src/main/java/com/project/underline/post/entity/repository/PostRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> , PostRepositoryCustom{
     Optional<Post> findByPostId(Long postId);
 
     Page<Post> findAll(Pageable pageable);
