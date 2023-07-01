3ed461fff0b75d2413a23a5600836f02ef3fb095
wanjongth <infinity4869@gmail.com>
2023-07-01 16:40:11 +0900

    배포 설정 추가

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 sudo docker pull infinity4869/underline-app
-sudo nohup docker run -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -e TZ=Asia/Seoul --name=underline-app infinity4869/underline-app > nohup_test.out 2>&1 &
+mkdir $(date '+%y%m%d')
+sudo nohup docker run -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -e TZ=Asia/Seoul --name=underline-app infinity4869/underline-app > $(date '+%y%m%d')/log.out 2>&1 &
