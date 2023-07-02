475c04b16a2adbce4bdbf59d2461b71632230a58
wanjongth <infinity4869@gmail.com>
2023-07-02 16:42:49 +0900

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 sudo docker pull infinity4869/underline-app
-mkdir $(date '+%y%m%d')
+if [ ! -d $(date '+%y%m%d') ]; then
+  mkdir $(date '+%y%m%d')
+fi
 sudo nohup docker run -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -e TZ=Asia/Seoul --name=underline-app infinity4869/underline-app > $(date '+%y%m%d')/log.out 2>&1 &
